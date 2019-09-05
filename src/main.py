ca556a0844fef802f9a5ea50c4cffc48cf70878e
zguilian <35277198+zguilian@users.noreply.github.com>
2019-09-05 15:41:46 -0400

    Formula Added
    simple formula calculate advisory speed added

--- src/main.py
+++ src/main.py
@@ -7,9 +7,13 @@ if __name__ == '__main__':
 
     # Create system
     system1 = CalculateSpeed(1, 1, 1)
+    #Super Elevation(Percent), Side Friction Factor, Radius of Curvature (ft)
+        #Super Elevation: the amount by which the outer edge of a curve on a road or railroad is banked above the inner edge.
+        #Side Friction Factor: represents the friction between the tires and pavement surface. This friction results in a lateral acceleration that acts upon a vehicle, and which occupants within the vehicle can feel. Like superelevation, side friction factor is limited for design speeds.
+
 
     # Calculate speed
     new_speed = system1.calculate_speed()
-
+    #new speed is advisory speed, mph
 
     print("Truck speed should be: " + str(new_speed))
