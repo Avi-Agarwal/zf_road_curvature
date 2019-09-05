9c3d843039fd1f16ca473b0cc7ff48f9710cba22
zguilian <35277198+zguilian@users.noreply.github.com>
2019-09-05 15:41:46 -0400

    Formula Added
    simple formula calculate advisory speed added

--- src/calculate_speed.py
+++ src/calculate_speed.py
@@ -1,14 +1,15 @@
 
 class CalculateSpeed:
 
-    def __init__(self, curvature, weight, length):
-        self.curvature = curvature
-        self.weight = weight
-        self.length = length
+    def __init__(self, SuperElevation, SideFrictionFactor, CurvatureRadius):
+        self.elevation = SuperElevation     #Percent
+        self.friction = SideFrictionFactor    #no unit
+        self.curvature = CurvatureRadius       #ft
 
     def calculate_speed(self):
-        return 10
-
+        return 15*(0.01*self.elevation+self.friction)*self.curvature
+        # V^2 = 15*(0.01*super elevation + side friction factor)* radius of curvature
+        # Advisory speed (MPH)
 # Main Function
 if __name__ == '__main__':
     system1 = CalculateSpeed(1,1,1)
