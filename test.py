35f592f765eb98e9179cb3a51cf98b883567214f
Avi Agarwal <avi7agarwal@gmail.com>
2019-09-05 14:16:58 -0400

    hey first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+# Test Class
+class Test:
+
+    def test_function(self):
+        print('ZF team best team')
+
+# Class runner
+if __name__ == '__main__':
+    test1 = Test()
+    test1.test_function()
