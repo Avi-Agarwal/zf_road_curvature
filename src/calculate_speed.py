2ebf20ff43bcb921acb40f89aa8dafd893848d62
Avi Agarwal <avi7agarwal@gmail.com>
2019-09-09 23:34:28 -0400

    changes

--- src/calculate_speed.py
+++ src/calculate_speed.py
@@ -1,3 +1,4 @@
+import math
 
 class CalculateSpeed:
 
@@ -7,7 +8,9 @@ class CalculateSpeed:
         self.curvature = CurvatureRadius       #ft
 
     def calculate_speed(self):
-        return 15*(0.01*self.elevation+self.friction)*self.curvature
+        return math.sqrt(15 * (0.01 * self.elevation + self.friction) * self.curvature)
+            # V (mph) ^2 = 15 * (0.01 * superelevation (%) + side friction factor) * radius of curvature (ft)
+        # return 15*(0.01*self.elevation+self.friction)*self.curvature
         # V^2 = 15*(0.01*super elevation + side friction factor)* radius of curvature
         # Advisory speed (MPH)
 # Main Function
@@ -15,3 +18,17 @@ if __name__ == '__main__':
     system1 = CalculateSpeed(1,1,1)
     new_speed = system1.calculate_speed()
     print("Truck speed should be: " + str(new_speed))
+
+import math
+
+
+class CalculateSpeed:
+
+    def __init__(self, SuperElevation, SideFrictionFactor, CurvatureRadius):
+        self.elevation = SuperElevation  # Percent
+        self.friction = SideFrictionFactor  # no unit
+        self.curvature = CurvatureRadius  # ft
+
+
+
+
