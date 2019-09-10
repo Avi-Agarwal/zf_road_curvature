a56a99709efb54ce263175e76a974c983588f328
Avi Agarwal <avi7agarwal@gmail.com>
2019-09-10 00:48:36 -0400

    working with local

--- src/first_detection.py
+++ src/first_detection.py
@@ -8,6 +8,8 @@ detector.setModelTypeAsRetinaNet()
 detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
 detector.loadModel()
 detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "cam_view.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
+# Creates local images of objects found
+# detections, extracted_images = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"), extract_detected_objects=True)
 
 for eachObject in detections:
     print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
