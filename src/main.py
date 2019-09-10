da3ce3619b5b49a618008c4e4590b120238c0d22
Avi Agarwal <avi7agarwal@gmail.com>
2019-09-10 13:57:24 -0400

    idk

--- src/main.py
+++ src/main.py
@@ -19,22 +19,4 @@ if __name__ == '__main__':
     advisory_speed = system1.calculate_speed()
     # advisory_speed is in mph
 
-    print('Truck speed should be: ' + str(advisory_speed) + ' mph')
-# # Main Function
-# if __name__ == '__main__':
-# #
-# #
-#     # Find system variables for now us temp variables
-#
-#     # Create system
-#     system1 = CalculateSpeed(1, 1, 1)
-#     #Super Elevation(Percent), Side Friction Factor, Radius of Curvature (ft)
-#         #Super Elevation: the amount by which the outer edge of a curve on a road or railroad is banked above the inner edge.
-#         #Side Friction Factor: represents the friction between the tires and pavement surface. This friction results in a lateral acceleration that acts upon a vehicle, and which occupants within the vehicle can feel. Like superelevation, side friction factor is limited for design speeds.
-#
-#
-#     # Calculate speed
-#     new_speed = system1.calculate_speed()
-#     #new speed is advisory speed, mph
-#
-#     print("Truck speed should be: " + str(new_speed))
+    print('Advised Truck speed should be: ' + str(advisory_speed) + ' mph')
